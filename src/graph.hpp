fa8c07b9fc95fb222e23db682eb2ff51a7be7d40
JakubCygaro <57292664+JakubCygaro@users.noreply.github.com>
2025-07-28 20:24:12 +0200

    testing connetion on randomized graphs with dfs and bfs

--- src/graph.hpp
+++ src/graph.hpp
@@ -1,101 +1,145 @@
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
 
+#include <deque>
 #include <list>
 #include <tuple>
 #include <vector>
 #include <cstdio>
 #include <stdexcept>
+#include <stack>
+namespace gr {
 
-template <typename T>
-class Graph {
-public:
-    struct Node;
-    struct Edge;
+    template <typename T>
+    class Graph {
+    public:
+        struct Node;
+        struct Edge;
 
-    struct Node {
-        std::list<Edge*> edges{};
-        T node_data;
-    };
-    struct Edge {
-        Node* tail{};
-        Node* head{};
-    };
+        struct Node {
+            std::list<Edge*> edges{};
+            T node_data;
+        };
+        struct Edge {
+            Node* tail{};
+            Node* head{};
+        };
 
-    using node_t = Node;
-    using edge_t = Edge;
-    using node_data_t = T;
-    using vmatrix = std::vector<std::tuple<node_data_t, std::vector<int>>>;
-public:
-    std::list<Node> nodes{};
-    std::list<Edge> edges{};
+        using node_t = Node;
+        using edge_t = Edge;
+        using node_data_t = T;
+        using vmatrix = std::vector<std::tuple<node_data_t, std::vector<int>>>;
+    public:
+        std::list<Node> nodes{};
+        std::list<Edge> edges{};
 
-    template<int N>
-    inline static Graph<T> from_matrix(std::array<std::tuple<T, std::array<int, N>>, N> mtx) {
-        static_assert(std::is_default_constructible<T>(), "T is not default constructible");
-        std::vector<Graph<T>::Node*> nodes_v(N);
-        std::list<Graph<T>::Node> nodes{};
-        std::list<Graph<T>::Edge> edges{};
+        template<int N>
+        inline static Graph<T> from_matrix(std::array<std::tuple<T, std::array<int, N>>, N> mtx) {
+            static_assert(std::is_default_constructible<T>(), "T is not default constructible");
+            std::vector<Graph<T>::Node*> nodes_v(N);
+            std::list<Graph<T>::Node> nodes{};
+            std::list<Graph<T>::Edge> edges{};
 
-        //create nodes
-        for (auto i = 0; i < N; i++){
-            nodes.push_back(Graph<T>::Node{.edges = {}, .node_data = std::get<0>(mtx[i])});
-            nodes_v[i] = &nodes.back();
+            //create nodes
+            for (auto i = 0; i < N; i++){
+                nodes.push_back(Graph<T>::Node{.edges = {}, .node_data = std::get<0>(mtx[i])});
+                nodes_v[i] = &nodes.back();
+            }
+
+
+            for (auto node_a = 0; node_a < N; node_a++) {
+                for(auto node_b = 0; node_b < N; node_b++) {
+                    //skip if this is the same node
+                    if (node_a == node_b) continue;
+
+                    if (std::get<1>(mtx[node_a])[node_b] == 1){
+                        edges.push_back(Graph<T>::Edge { .tail = nodes_v[node_a], .head = nodes_v[node_b] });
+                        const auto edge = &edges.back();
+                        nodes_v[node_a]->edges.push_back(edge);
+                    }
+                }
+            }
+            return Graph<T>{
+                .nodes = std::move(nodes),
+                .edges = std::move(edges)
+            };
         }
+        inline static Graph<T> from_matrix(std::vector<std::tuple<T, std::vector<int>>> mtx) {
+            static_assert(std::is_default_constructible<T>(), "T is not default constructible");
+            if(mtx.size() != std::get<1>(mtx[0]).size()) {
+                throw std::runtime_error("the matrix must be a square matrix");
+            }
+            auto const N = mtx.size();
+            std::vector<Graph<T>::Node*> nodes_v(N);
+            std::list<Graph<T>::Node> nodes{};
+            std::list<Graph<T>::Edge> edges{};
+
+            //create nodes
+            for (auto i = 0; i < N; i++){
+                nodes.push_back(Graph<T>::Node{.edges = {}, .node_data = std::get<0>(mtx[i])});
+                nodes_v[i] = &nodes.back();
+            }
 
 
-        for (auto node_a = 0; node_a < N; node_a++) {
-            for(auto node_b = 0; node_b < N; node_b++) {
-                //skip if this is the same node
-                if (node_a == node_b) continue;
+            for (auto node_a = 0; node_a < N; node_a++) {
+                for(auto node_b = 0; node_b < N; node_b++) {
+                    //skip if this is the same node
+                    if (node_a == node_b) continue;
 
-                if (std::get<1>(mtx[node_a])[node_b] == 1){
-                    edges.push_back(Graph<T>::Edge { .tail = nodes_v[node_a], .head = nodes_v[node_b] });
-                    const auto edge = &edges.back();
-                    nodes_v[node_a]->edges.push_back(edge);
+                    if (std::get<1>(mtx[node_a])[node_b] == 1){
+                        edges.push_back(Graph<T>::Edge { .tail = nodes_v[node_a], .head = nodes_v[node_b] });
+                        const auto edge = &edges.back();
+                        nodes_v[node_a]->edges.push_back(edge);
+                    }
+                }
+            }
+            return Graph<T>{
+                .nodes = std::move(nodes),
+                .edges = std::move(edges)
+            };
+        }
+    };
+    class ExplorableGraphData {
+    public:
+        bool explored{};
+    };
+    template <typename T, typename N = Graph<T>::node_t>
+    inline void dfs(const Graph<T>& graph, N* start) {
+        static_assert(std::is_convertible<T*, ExplorableGraphData*>::value, "T must be derived from ExplorableGraphData");
+        std::stack<N*> nodes{};
+        nodes.push(start);
+
+        while(!nodes.empty()) {
+            auto v = nodes.top();
+            auto* e_ex = static_cast<ExplorableGraphData*>(&v->node_data);
+            nodes.pop();
+            if (!e_ex->explored) {
+                e_ex->explored = true;
+                for(auto& edge : v->edges) {
+                    nodes.push(edge->head);
                 }
             }
         }
-        return Graph<T>{
-            .nodes = std::move(nodes),
-            .edges = std::move(edges)
-        };
     }
-    inline static Graph<T> from_matrix(std::vector<std::tuple<T, std::vector<int>>> mtx) {
-        static_assert(std::is_default_constructible<T>(), "T is not default constructible");
-        if(mtx.size() != std::get<1>(mtx[0]).size()) {
-            throw std::runtime_error("the matrix must be a square matrix");
-        }
-        auto const N = mtx.size();
-        std::vector<Graph<T>::Node*> nodes_v(N);
-        std::list<Graph<T>::Node> nodes{};
-        std::list<Graph<T>::Edge> edges{};
+    template <typename T, typename N = Graph<T>::node_t>
+    inline void bfs(const Graph<T>& graph, N* start) {
+        static_assert(std::is_convertible<T*, ExplorableGraphData*>::value, "T must be derived from ExplorableGraphData");
+        std::deque<N*> nodes{};
+        nodes.push_back(start);
 
-        //create nodes
-        for (auto i = 0; i < N; i++){
-            nodes.push_back(Graph<T>::Node{.edges = {}, .node_data = std::get<0>(mtx[i])});
-            nodes_v[i] = &nodes.back();
-        }
-
-
-        for (auto node_a = 0; node_a < N; node_a++) {
-            for(auto node_b = 0; node_b < N; node_b++) {
-                //skip if this is the same node
-                if (node_a == node_b) continue;
-
-                if (std::get<1>(mtx[node_a])[node_b] == 1){
-                    edges.push_back(Graph<T>::Edge { .tail = nodes_v[node_a], .head = nodes_v[node_b] });
-                    const auto edge = &edges.back();
-                    nodes_v[node_a]->edges.push_back(edge);
+        while(!nodes.empty()) {
+            auto v = nodes.front();
+            auto* e_ex = static_cast<ExplorableGraphData*>(&v->node_data);
+            nodes.pop_front();
+            if (!e_ex->explored) {
+                e_ex->explored = true;
+                for(auto& edge : v->edges) {
+                    nodes.push_back(edge->head);
                 }
             }
         }
-        return Graph<T>{
-            .nodes = std::move(nodes),
-            .edges = std::move(edges)
-        };
     }
-};
+}
 
 
 #endif
---
+++ src/graph_connection.cc
@@ -0,0 +1,153 @@
+#include "graph.hpp"
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <format>
+#include <array>
+#include <ostream>
+#include <tuple>
+#include "common.hpp"
+
+using namespace gr;
+
+struct GraphData : public gr::ExplorableGraphData {
+    int id{};
+
+    GraphData(int n) : id(n) {}
+    GraphData(){}
+
+    decltype(std::cout)& operator<<(decltype(std::cout)& os) {
+        os << std::format("[ {} = {} ]", this->id, this->explored);
+        return os;
+    }
+    decltype(std::cout)& operator>>(decltype(std::cout)& os) {
+        os << this;
+        return os;
+    }
+};
+
+void test_if_connected(std::function<void(const gr::Graph<GraphData>&, gr::Graph<GraphData>::node_t*)> check_conn_fn) {
+
+    const auto vertex_n = common::get_random_in_range(3, 50);
+
+    // how many vertices have to be connected
+    const auto connected_v = common::get_random_in_range(1, vertex_n);
+
+    gr::Graph<GraphData> graph{};
+
+    using node_t = decltype(graph)::node_t;
+    // keep track of all the vertices
+    std::vector<node_t*> vertices(vertex_n);
+
+    for (auto i = 0; i < vertex_n; i++) {
+        node_t new_node = {
+            .edges = {},
+            .node_data = GraphData(i),
+        };
+        graph.nodes.emplace_back(new_node);
+        // store this vertex
+        vertices[i] = &graph.nodes.back();
+    }
+    // generate random connections
+    for (auto i = 1; i < vertex_n; i++) {
+        if (i < connected_v) {
+            graph.edges.push_back({
+                        .tail = vertices[i - 1],
+                        .head = vertices[i]
+                        });
+            vertices[i]->edges.push_back(&graph.edges.back());
+            vertices[i-1]->edges.push_back(&graph.edges.back());
+            graph.edges.push_back({
+                        .tail = vertices[i],
+                        .head = vertices[i - 1]
+                        });
+            vertices[i]->edges.push_back(&graph.edges.back());
+            vertices[i-1]->edges.push_back(&graph.edges.back());
+
+            // for each further vertex there is a 30 percent chance that it will be connected with the current one
+            for (auto k = 2; k < connected_v - i ; k++) {
+                if(common::get_random_in_range(1, 100) > 15) continue;
+
+                graph.edges.push_back({
+                            .tail = vertices[i + k],
+                            .head = vertices[i]
+                            });
+                vertices[i]->edges.push_back(&graph.edges.back());
+                vertices[i + k]->edges.push_back(&graph.edges.back());
+                graph.edges.push_back({
+                            .tail = vertices[i],
+                            .head = vertices[i + k]
+                            });
+                vertices[i]->edges.push_back(&graph.edges.back());
+                vertices[i + k]->edges.push_back(&graph.edges.back());
+            }
+        } else {
+            auto random_to_connect = common::get_random_in_range(i + 1, vertex_n - 1);
+            if (random_to_connect > vertex_n || random_to_connect <= i) break;
+
+            if (common::get_random_in_range(1, 100) > 50) {
+                graph.edges.push_back({
+                            .tail = vertices[random_to_connect],
+                            .head = vertices[i]
+                            });
+                vertices[i]->edges.push_back(&graph.edges.back());
+                graph.edges.push_back({
+                            .tail = vertices[i],
+                            .head = vertices[random_to_connect]
+                            });
+                vertices[i]->edges.push_back(&graph.edges.back());
+            }
+        }
+    }
+
+    // test random graph
+    for (auto v_i = 0; v_i < vertices.size(); v_i++) {
+        // mark all as unexplored
+        for (auto& vertex : graph.nodes) {
+            vertex.node_data.explored = false;
+        }
+
+        gr::dfs(graph, vertices[v_i]);
+
+        for (auto i = 0; i < vertices.size(); i++) {
+            assert(i < connected_v && v_i < connected_v ?
+                    vertices[i]->node_data.explored :
+                    i >= connected_v && v_i < connected_v ?
+                        !vertices[i]->node_data.explored :
+                        true && "node connected but should not be");
+        }
+
+    }
+    // Graph<GraphData>::vmatrix matrix(n);
+    //
+    // for (auto i = 0; i < n; i++){
+    //     GraphData gd{i};
+    //     std::vector<int> row(n);
+    //
+    //     std::for_each(row.begin() + i + 1, row.end(), [&](auto& c) {
+    //         c = common::get_random_in_range(0, 1);
+    //     });
+    //
+    //     matrix[i] = std::make_tuple(gd, std::move(row));
+    // }
+    // for (auto r = 0; r < n; r++) {
+    //     auto& row = std::get<1>(matrix[r]);
+    //     for (auto c = 0; c < n; c++) {
+    //         std::get<1>(matrix[c])[r] = row[c];
+    //     }
+    // }
+    //
+    // auto graph = gr::Graph<GraphData>::from_matrix(matrix);
+    //
+    // int i{};
+    // for (auto it = graph.nodes.begin(); it != graph.nodes.end(); it++) {
+    //
+    //     i++;
+    // }
+}
+int main(void) {
+    for (auto i = 0; i < 100; i++) {
+        test_if_connected(gr::dfs<GraphData>);
+        test_if_connected(gr::bfs<GraphData>);
+    }
+}
