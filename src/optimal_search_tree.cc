5520e6adb3965193ad26c1a22494fcbf802102f4
papieros <robertrugala@gmail.com>
2025-11-22 14:11:46 +0100

    opttree fixed?

--- src/optimal_search_tree.cc
+++ src/optimal_search_tree.cc
@@ -1,5 +1,5 @@
 #include <algorithm>
-#include <limits>
+#include <cassert>
 #include <print>
 #include <vector>
 struct Node {
@@ -10,35 +10,53 @@ struct Node {
 
 int main(void) {
     std::vector<Node*> nodes = {
-        new Node(10, 2),
+        new Node(1, 2),
         new Node(2, 3),
-        new Node(20, 6),
-        new Node(5, 1),
-        new Node(1, 0),
+        new Node(3, 6),
+        new Node(4, 1),
+        new Node(5, 12),
     };
-
     const size_t cache_sz = nodes.size() + 1;
     const size_t N = nodes.size();
 
+    std::sort(nodes.begin(), nodes.end(), [](auto a, auto b) { return a->key < b->key; });
+
     int cache[cache_sz][cache_sz];
+
+    for(auto i = 0ul; i < cache_sz; i++) {
+        for(auto j = 0ul; j < cache_sz; j++) {
+            cache[i][j] = 0;
+        }
+    }
 
     for(size_t i = 1; i <= cache_sz; i++)
         cache[i][i - 1] = 0;
 
-    for(size_t s = 0; s <= N - 1; s++){
+    for(size_t s = 0; s < N; s++){
         for(size_t i = 1; i <= N - s; i++){
-            float freq_sum{};
-            for(auto k = i; k < i + s; k++){
-                freq_sum += nodes[k]->freq;
+            int freq_sum{};
+            for(auto k = i; k <= i + s; k++){
+                freq_sum += nodes[k - 1]->freq;
             }
-            int min = std::numeric_limits<int>::max();
+            int min = cache[i][i];
             for(auto r = i; r < i + s; r++){
-                min = std::min(min, cache[i][r-1] + cache[r+1][i+s]);
+                assert(i < cache_sz);
+                assert(r-1 < cache_sz);
+                assert(r+1 < cache_sz);
+                assert(i+s < cache_sz);
+                auto tmp = cache[i][r-1] + cache[r+1][i+s];
+                min = std::min(min, tmp);
             }
             cache[i][i+s] = freq_sum + min;
         }
     }
 
+    for(auto i = 0ul; i < cache_sz; i++) {
+        for(auto j = 0ul; j < cache_sz; j++) {
+            std::print("\t{} ", cache[i][j]);
+        }
+        std::println();
+    }
     auto sol = cache[1][N];
 
     std::println("{}", sol);
