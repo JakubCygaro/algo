e762c402f4593e03d0ea6f4056716bddfbc806e4
papieros <robertrugala@gmail.com>
2026-02-28 11:10:27 +0100

    whatever

--- src/rod_cutting.cc
+++ src/rod_cutting.cc
@@ -1,30 +1,40 @@
+#include <algorithm>
+#include <cassert>
+#include <common.hpp>
+#include <cstddef>
 #include <limits>
 #include <print>
 #include <vector>
 int main(void)
 {
-    std::vector p = { 0, 2, 3, 1, 10, 5, 6, 7, 10, 12, 11 };
-    const auto n = 10;
+    const std::size_t n = common::get_random_in_range(8, 24);
+    std::vector<int> p(n + 1);
+    std::ranges::for_each(p, [](auto& e) {
+        e = common::get_random_in_range(1, 32);
+    });
+    p[0] = 0;
     std::vector<int> r(n + 1);
     r[0] = 0;
+    std::vector<int> s(n + 1);
+    std::println("{}", p);
     for (auto j = 1ul; j < r.size(); j++) {
         auto q = std::numeric_limits<int>::min();
         for (auto i = 1ul; i <= j; i++) {
-            q = std::max(q, p[i] + p[j - i]);
+            if (q < p[i] + r[j - i]){
+                s[j] = i;
+                q = p[i] + r[j - i];
+            }
         }
         r[j] = q;
     }
-    std::println("{}", r);
-    std::vector<int> sol {};
-    for (auto j = (int)r.size() - 1; j > 0;) {
-        auto rvn = r[j];
-        for (auto i = 0; i <= j; i++) {
-            if (p[i] + p[j - i] == rvn) {
-                sol.push_back(j-i);
-                j = i;
-                break;
-            }
-        }
+    std::vector<int> sol;
+    auto total_r = 0;
+    for(auto len = (int)n; len > 0; ){
+        sol.push_back(s[len]);
+        len = len - s[len];
     }
-    std::println("{}", sol);
+    for(auto idx : sol){
+        total_r += p[idx];
+    }
+    assert(total_r == r[n]);
 }
